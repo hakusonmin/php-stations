dcd048ed35b6090c2b35088b3dffa76ec065509a
hakusonmin <fujitakosei7@gmail.com>
2025-03-11 20:48:38 +0900

    Station17

--- memo.md
+++ memo.md
@@ -1,2 +1,9 @@
 
 `docker compose exec php-container vendor/bin/phpunit tests/Station07/QuestionTest.php`
+
+
+最後のStationは
+
+ /app/vendor/bin/php-cs-fixer fix /app/src/Station18/Question/Question.php
+
+ を phpのコンテナ内で実行しました.. (exec -it で入った状態で..)
--- src/Station18/Question/.php-cs-fixer.php
+++ src/Station18/Question/.php-cs-fixer.php
@@ -3,8 +3,25 @@
 $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
 return (new PhpCsFixer\Config())
-    ->setRules([
-        '@PSR2' => true,
-    ])
-    ->setUsingCache(false)
-    ->setFinder($finder);
+  ->setRules([
+    '@PSR2' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'no_useless_else' => true,
+    'ordered_class_elements' => [
+      'order' => [
+        'use_trait',
+        'constant_public',
+        'constant_protected',
+        'constant_private',
+        'property_public',
+        'property_protected',
+        'property_private',
+        'construct',
+        'method_public',
+        'method_protected',
+        'method_private'
+      ],
+    ]
+  ])
+  ->setUsingCache(false)
+  ->setFinder($finder);
